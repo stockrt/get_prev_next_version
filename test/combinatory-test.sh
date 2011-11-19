a41473c6e4b7c129786b7170b8e645c12b9d6edb
Rogério Carvalho Schneider <stockrt@gmail.com>
2011-11-19 02:32:25 -0200

    Removing unused variables

--- test/combinatory-test.sh
+++ test/combinatory-test.sh
@@ -29,13 +29,12 @@
 #############
 
 err_field_size="ERR: Verify field size and version number coherency."
-usage_message="Usage: get_prev_next_version.sh 2 prev 1.1.1
-Usage: get_prev_next_version.sh 2 next 1.1.1"
 
 
 ###########
 ## TESTS ##
 ###########
+
 
 ################
 ## Backward 1 ##
